f579e407d8f94d7cbac9379d084c12cb228f235a
David Guzman <davidguzmaninc@gmail.com>
2021-03-12 22:53:33 -0500

    Added a sticky nav

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,29 @@
-AOS.init();
+// Animation library that allows you to create all sorts of animations
+const animationOnScroll = () => {
+    AOS.init();
+};
+
+// When the user scrolls the nav will have a fixed position on the top
+const stickyNav = () => {
+    const nav = document.querySelector('.navbar');
+    const topOfNav = nav.offsetTop;
+
+    const fixNav = () => {
+        if (window.scrollY >= 81) {
+            nav.classList.add('fixed-nav')
+        }
+        else {
+            nav.classList.remove('fixed-nav')
+        }
+    };
+
+    window.addEventListener('scroll', fixNav);
+};
+
+// Initialize all my functions
+const app = () => {
+    animationOnScroll();
+    stickyNav();
+};
+
+app();
